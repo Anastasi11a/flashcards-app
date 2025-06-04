a783d95d6dc91f4f16c4da57f95c262b2767d931
Anastasiia <felitsias22@gmail.com>
2025-06-04 17:33:08 +0200

    Configured navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import { View, useColorScheme } from "react-native";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import styled, { ThemeProvider } from "styled-components";
+
+import { lightTheme, darkTheme } from "@/themes/theme";
 
 export default function RootLayout() {
-  return <Stack />;
+    const colorScheme = useColorScheme();
+    const theme = colorScheme === 'dark' ? darkTheme : lightTheme;
+
+    return (
+        <ThemeProvider theme={theme}>
+            <StyledView>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    <Stack.Screen name="+not-found" />
+                </Stack>
+                <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+            </StyledView>
+        </ThemeProvider>
+    );
 }
+
+const StyledView = styled(View)`
+    display: flex;
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.background};
+`;
