4796b1c02c768a6889686179f379af74d4a065dd
Anastasiia <felitsias22@gmail.com>
2025-06-07 22:51:10 +0200

    Implemented AddDeck page

--- app/add-deck.tsx
+++ app/add-deck.tsx
@@ -1,56 +1,10 @@
-import { useState } from "react";
-import { useRouter } from "expo-router";
-import { View, Text, TextInput, Button, Alert } from "react-native";
-import styled from "styled-components/native";
+import AddDeckScreen from "@/components/AddDeckScreen";
+import useCustomHeader from "@/hooks/useCustomHeader";
 
-const AddDeckScreen = () => {
-    const router = useRouter();
-    const [title, setTitle] = useState('');
+const AddDeck = () => {
+    useCustomHeader({ title: 'Add New Deck' });
 
-    const handleAdd = () => {
-        if (title.trim() === '') {
-            Alert.alert("Validation", "Please enter a deck title.");
-            return;
-        }
-        console.log("New deck title:", title);
-
-        router.back();
-    };
-
-    return (
-        <StyledView>
-            <StyledTitle>Deck Title</StyledTitle>
-            <StyledInput
-                value={title}
-                placeholder='Enter deck title'
-                placeholderTextColor='#aaa'
-                onChangeText={setTitle}
-            />
-            <Button title='Add Deck' onPress={handleAdd} />
-        </StyledView>
-    );
+    return <AddDeckScreen />
 };
 
-export default AddDeckScreen;
-
-const StyledView = styled(View)`
-    flex: 1;
-    padding: 24px;
-    background-color: #25292e;
-`;
-
-const StyledTitle = styled(Text)`
-    margin-bottom: 8px;
-    font-size: 18px;
-    color: #e6e6e6;
-`;
-
-const StyledInput = styled(TextInput)`
-    margin-bottom: 16px;
-    height: 48px;
-    border-radius: 12px;
-    padding: 12px;
-    font-size: 16px;
-    background-color: #1a1c20;
-    color: #fff;
-`;
+export default AddDeck;
