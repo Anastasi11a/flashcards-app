695f2d08caeaf8bb33af721ea3922d4b57158d4d
Anastasiia <felitsias22@gmail.com>
2025-07-17 11:53:16 +0200

    Added component for handling question and answer inputs

---
+++ components/CardInputs.tsx
@@ -0,0 +1,39 @@
+import { TextInput } from "react-native";
+
+import InputField from "./InputField";
+import { AnswerInput, Divider, InputWrapper } from "@/ui/CardInputFields";
+import QuestionInput from "@/ui/QuestionInput";
+
+interface CardInputsProps {
+    inputRef?: React.Ref<TextInput>;
+    question: string;
+    answer: string;
+    onChangeQuestion: (text: string) => void;
+    onChangeAnswer: (text: string) => void;
+}
+
+const CardInputs = ({ 
+    inputRef, question, answer, onChangeQuestion, onChangeAnswer
+}: CardInputsProps) => {
+    return (        
+        <InputWrapper>
+            <InputField
+                inputRef={inputRef}
+                text={question}
+                InputComponent={QuestionInput}
+                placeholder='Type a question or something else'
+                maxLengthHint={75}
+                onChangeText={onChangeQuestion}
+            />
+            <Divider />
+            <InputField
+                text={answer}
+                InputComponent={AnswerInput}
+                placeholder='Type a description or something else'
+                onChangeText={onChangeAnswer}
+            />
+        </InputWrapper>
+    );
+};
+
+export default CardInputs;
