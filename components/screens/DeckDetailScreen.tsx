5001472ca9b924e43506f5615c93aa7a0022c0bd
Anastasiia <felitsias22@gmail.com>
2025-07-20 18:09:20 +0200

    Updated component for managing DeckList height

--- components/screens/DeckDetailScreen.tsx
+++ components/screens/DeckDetailScreen.tsx
@@ -1,9 +1,9 @@
 import { useCallback } from "react";
 import { useRouter } from "expo-router";
-import { useHeaderHeight } from "@react-navigation/elements";
 
 import DeckList from "../DecksList";
 import MenuPopupButton from "../MenuPopupButton";
+import DeckListContainer from "../DeckListContainer";
 import { useDecks } from "@/context/DeckContext";
 import { useDeckMenuButtons } from "@/hooks/useDeckMenuButtons";
 import { useConfirmDeleteDeck } from "@/hooks/useConfirmDeleteDeck";
@@ -17,8 +17,6 @@ interface DeckDetailScreenProps {
 
 const DeckDetailScreen = ({ deckId, isMenuVisible, onCloseMenu }: DeckDetailScreenProps) => {
     const router = useRouter();
-    const headerHeight = useHeaderHeight();
-
     const { decks, deleteCard } = useDecks();
     const deck = decks.find((d) => d.id === deckId);
     if (!deck || !deckId) return null;
@@ -76,13 +74,14 @@ const DeckDetailScreen = ({ deckId, isMenuVisible, onCloseMenu }: DeckDetailScre
                 buttons={menuButtons()} 
                 onClose={onCloseMenu}
             />
-            <DeckList 
-                deckId={deckId}
-                cards={deck.cards}
-                paddingTop={headerHeight}
-                onDelete={handleDeleteCard}
-                onEdit={(_, cardId) => handleEditCardPressed(cardId)}
-            />
+            <DeckListContainer>
+                <DeckList 
+                    deckId={deckId}
+                    cards={deck.cards}
+                    onDelete={handleDeleteCard}
+                    onEdit={(_, cardId) => handleEditCardPressed(cardId)}
+                />
+            </DeckListContainer>
         </>
     );
 };
