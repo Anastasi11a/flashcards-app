da9b59366de7f226cb36c13dabb611f5f9a20fd2
Anastasiia <felitsias22@gmail.com>
2025-07-25 15:37:59 +0200

    Divided into functional component and UI components

--- components/CardInputs.tsx
+++ components/CardInputs.tsx
@@ -1,38 +1,16 @@
-import { TextInput } from "react-native";
-
-import InputField from "./InputField";
-import { AnswerInput, Divider, InputWrapper } from "@/ui/CardInputFields";
-import QuestionInput from "@/ui/QuestionInput";
-
-interface CardInputsProps {
-    inputRef?: React.Ref<TextInput>;
-    question: string;
-    answer: string;
-    onChangeQuestion: (text: string) => void;
-    onChangeAnswer: (text: string) => void;
-}
+import CardInputsView, { CardInputsViewProps } from "@/ui/CardInputsView";
 
 const CardInputs = ({ 
     inputRef, question, answer, onChangeQuestion, onChangeAnswer
-}: CardInputsProps) => {
-    return (        
-        <InputWrapper>
-            <InputField
-                inputRef={inputRef}
-                value={question}
-                InputComponent={QuestionInput}
-                placeholder='Type a question or something else'
-                maxLengthHint={75}
-                onChangeText={onChangeQuestion}
-            />
-            <Divider />
-            <InputField
-                value={answer}
-                InputComponent={AnswerInput}
-                placeholder='Type a description or something else'
-                onChangeText={onChangeAnswer}
-            />
-        </InputWrapper>
+}: CardInputsViewProps) => {
+    return (    
+        <CardInputsView
+            inputRef={inputRef}
+            question={question}
+            answer={answer}
+            onChangeQuestion={onChangeQuestion}
+            onChangeAnswer={onChangeAnswer}
+        /> 
     );
 };
 
