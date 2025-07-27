2297f5001de64af9237d502b6783870b739e4003
Anastasiia <felitsias22@gmail.com>
2025-07-27 15:38:51 +0200

    Divided into functional component and UI components

---
+++ components/AddCardsContainer.tsx
@@ -0,0 +1,33 @@
+import { TextInput } from "react-native";
+
+import CardInputs from "./CardInputs";
+import AddCardButton from "@/ui/AddCardButton";
+import AddCardsView from "@/ui/layout/AddCardsView";
+
+interface AddCardsContainerProps {
+    inputRef: React.Ref<TextInput>;
+    question: string;
+    answer: string;
+    onChangeQuestion: (text: string) => void;
+    onChangeAnswer: (text: string) => void;
+    onSave: () => void;
+}
+
+const AddCardsContainer  = ({ 
+    inputRef, question, answer, onChangeQuestion, onChangeAnswer, onSave, 
+}: AddCardsContainerProps) => {
+    return (
+        <AddCardsView> 
+            <CardInputs
+                inputRef={inputRef}
+                question={question}
+                answer={answer}
+                onChangeQuestion={onChangeQuestion}
+                onChangeAnswer={onChangeAnswer}
+            />
+            <AddCardButton label='Add Card' onPress={onSave} />
+        </AddCardsView>
+    );
+};
+
+export default AddCardsContainer;
