83ac066cc3bad7dbe86ac892d4719e3f81ee4dff
Anastasiia <felitsias22@gmail.com>
2025-07-27 15:39:41 +0200

    Divided into functional component and UI components

--- components/screens/AddCardsScreen.tsx
+++ components/screens/AddCardsScreen.tsx
@@ -1,13 +1,11 @@
 import { useRouter } from "expo-router";
 
 import { useDecks } from "@/context/DeckContext";
-import AddCardButton from "../AddCardButton";
+import AddCardsContainer from "../AddCardsContainer";
 import DeckList from "../DecksList";
-import CardInputs from "../CardInputs";
 import useKeyboardVisibility from "@/hooks/useKeyboardVisibility";
 import { useAddCardState } from "@/hooks/useAddCardState";
-import { StyledEAddScreenView } from "@/ui/CardInputFields";
-import StyledKeyboardAvoidingView from "@/ui/StyledKeyboardAvoidingView";
+import KeyboardBehavior from "@/ui/layout/KeyboardBehavior";
 
 interface AddCardsScreenProps {
     deckId: string;
@@ -37,25 +35,22 @@ const AddCardsScreen = ({ deckId }: AddCardsScreenProps) => {
     };
 
     return (
-        <StyledKeyboardAvoidingView>
-            <StyledEAddScreenView> 
-                <CardInputs
-                    inputRef={inputRef}
-                    question={question}
-                    answer={answer}
-                    onChangeQuestion={setQuestion}
-                    onChangeAnswer={setAnswer}
-                />
-                <AddCardButton label='Add Card' onPress={save} />
-            </StyledEAddScreenView>
-            
+        <KeyboardBehavior>
+            <AddCardsContainer
+                inputRef={inputRef}
+                question={question}
+                answer={answer}
+                onChangeQuestion={setQuestion}
+                onChangeAnswer={setAnswer}
+                onSave={save}
+            />
             <DeckList 
                 deckId={deckId} 
                 cards={cards} 
                 onDelete={(_, cardId) => handleDeleteCard(cardId)}
                 onEdit={(_, cardId) => handleEditCard(cardId)}
             />
-        </StyledKeyboardAvoidingView>
+        </KeyboardBehavior>
     );
 };
 
