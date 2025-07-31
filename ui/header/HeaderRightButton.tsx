f940815d921ddcbf24ea637cd4f798a455433d85
Anastasiia <felitsias22@gmail.com>
2025-07-31 11:18:14 +0200

    Updated UI components

--- ui/header/HeaderRightButton.tsx
+++ ui/header/HeaderRightButton.tsx
@@ -1,32 +1,22 @@
-import {  
-    Text, 
-    TouchableOpacity,
-    StyleProp, 
-    ViewStyle, 
-    TextStyle 
-} from "react-native";
+import { Text, TouchableOpacity,StyleProp, TextStyle } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import styled from "styled-components/native";
 
-export interface HeaderRightButtonProps {
+export interface RightButtonProps {
     onPress: () => void;
     label?: string;
     iconName?: keyof typeof SimpleLineIcons.glyphMap;
-    style?: StyleProp<ViewStyle>;
     textStyle?: StyleProp<TextStyle>;
 }
 
-const HeaderRightButton = ({ 
-    onPress, label, iconName, style, textStyle 
-}: HeaderRightButtonProps) => {
+const HeaderRightButton = ({ onPress, label, iconName, textStyle }: RightButtonProps) => {
     return (
         <TouchableOpacity
             onPress={onPress}
-            style={style}
-            hitSlop={{ top: 10, bottom: 10, left: 20, right: 10 }}>
+            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}>
 
             {iconName ? (
-                <SimpleLineIcons name={iconName} size={18} color="#808080" />
+                <SimpleLineIcons name={iconName} size={18} color='#808080' />
             ) : (
                 <StyledLabel style={textStyle}>{label}</StyledLabel>
             )}
