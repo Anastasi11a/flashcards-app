a3582ec3279f3e96c0463865ae5de7172e721ac0
Anastasiia <felitsias22@gmail.com>
2025-08-04 16:51:42 +0200

    Simplified InputField component by removing unnecessary inputRef handling for QuestionInput

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,16 +1,6 @@
 import InputWrapper, { InputWrapperProps } from "@/ui/InputWrapper";
-import QuestionInput from "@/ui/QuestionInput";
 
 const InputField = (props: InputWrapperProps) => {
-    const { inputRef, InputComponent } = props;
-    const refToUse = InputComponent === QuestionInput ? inputRef : undefined;
-
-    return (
-        <InputWrapper
-            {...props}
-            inputRef={refToUse}
-        />
-    );
+    return <InputWrapper {...props} />;
 };
-
 export default InputField;
