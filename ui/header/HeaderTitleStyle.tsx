66ce89f3a0b12a04ab27157650dd54cb8d5c73e1
Anastasiia <felitsias22@gmail.com>
2025-08-08 10:20:14 +0200

    Added HeaderTitleStyle ui component

---
+++ ui/header/HeaderTitleStyle.tsx
@@ -0,0 +1,19 @@
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+interface HeaderTitleStyleProps {
+    title: string;
+}
+const HeaderTitleStyle = ({ title }: HeaderTitleStyleProps) => (
+    <StyledTitle>{title}</StyledTitle>
+);
+
+export default HeaderTitleStyle;
+
+const StyledTitle = styled(Text)`
+    margin-top: 10px;
+    margin-start: 12px;
+    font-size: 28px;
+    font-weight: bold;
+    color: #0a7ea4;
+`;
