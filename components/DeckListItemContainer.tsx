63bb0a6964ddc9fa86017ed9accfa2265169d90b
Anastasiia <felitsias22@gmail.com>
2025-08-14 16:01:59 +0200

    Added card counter function

--- components/DeckListItemContainer.tsx
+++ components/DeckListItemContainer.tsx
@@ -1,17 +1,21 @@
 import { useDecks } from "@/context/DeckContext";
 import DeckListItem, { DeckListItemProps } from "@/ui/DeckListItem";
 
-type ContainerProps = Omit<DeckListItemProps, 'isFavorite'> & {
+type ContainerProps = Omit<DeckListItemProps, 'isFavorite' | 'cardCount'> & {
     deckId: string;
 };
 
 const DeckListItemContainer = ({ deckId, ...rest }: ContainerProps) => {
-    const { isDeckFavorite } = useDecks();
+    const { isDeckFavorite, savedDecks } = useDecks();
+
+    const deck = savedDecks.find(d => d.id === deckId);
+    const cardCount = deck ? deck.cards.length : 0;
 
     return (
         <DeckListItem
             {...rest}
             isFavorite={isDeckFavorite(deckId)}
+            cardCount={cardCount}
             isBookmarksPage={true}
             showBookmarkIcon={false}
         />
