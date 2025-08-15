b66ad0f8bd63709077d8746d859a22c170217097
Anastasiia <felitsias22@gmail.com>
2025-08-15 15:39:21 +0200

    Added color constants

---
+++ constants/colors/gradient.ts
@@ -0,0 +1,14 @@
+export const CHECKLIST = {
+    ACTIVE: ['#474f59', '#25292e'] as [string, string],
+    INACTIVE: ['#64d1f5', '#0a7ea4'] as [string, string],
+};
+
+export const DECK_CONTAINER = {
+    ACTIVE: '#2f343a',
+    INACTIVE: '#25292e',
+} as const;
+
+export const CHECKBOX = {
+    CHECKED: '#64d1f5',
+    UNCHECKED: '#b3b3b3',
+} as const;
