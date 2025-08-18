f25b4ae303f705a6a825349e4fb1d1531d1e42b4
Anastasiia <felitsias22@gmail.com>
2025-08-18 23:09:59 +0200

    Added  ButtonGradientView component for customizable gradient buttons

---
+++ ui/ButtonGradientView.tsx
@@ -0,0 +1,59 @@
+import { TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import styled from "styled-components/native";
+
+import { GRADIENTS } from "@/constants/colors/gradient";
+
+type IconComponent = React.ElementType;
+type GradientVariant = keyof typeof GRADIENTS;
+
+export interface IconProps {
+    icon: IconComponent;
+    iconName: string;
+    iconSize?: number;
+    iconColor?: string;
+    gradientVariant?: GradientVariant;
+    buttonSize?: number;
+}
+
+interface ButtonGradientViewProps extends IconProps{
+    onPress: () => void;
+}
+
+const ButtonGradientView = ({ 
+    icon: Icon, 
+    iconName,
+    iconSize = 24, 
+    iconColor = '#fff', 
+    gradientVariant = 'BLUE',
+    buttonSize = 46,
+    onPress,
+}: ButtonGradientViewProps) => {
+    const gradientColors = GRADIENTS[gradientVariant];
+
+    return (
+        <GradientOverlay colors={gradientColors} size={buttonSize}>
+            <StyledPressable onPress={onPress}>
+                <Icon name={iconName} size={iconSize} color={iconColor} />
+            </StyledPressable>
+        </GradientOverlay>     
+    );
+};
+
+export default ButtonGradientView;
+
+const GradientOverlay = styled(LinearGradient)<{ size: number }>`
+    width: ${({ size }) => size}px;
+    height: ${({ size }) => size}px;
+    border-radius: 16px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledPressable = styled(TouchableOpacity)`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+`;
