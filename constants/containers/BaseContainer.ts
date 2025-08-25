31995384ca5b855745a6f1bfd7b12e144c03f9c5
Anastasiia <felitsias22@gmail.com>
2025-08-25 17:56:18 +0200

    Added BaseContainer UI component

---
+++ constants/containers/BaseContainer.ts
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+export const BaseContainer = styled(View)`
+    height: 70px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    background-color: #2f343a;
+`;
