15dfbccdf125d55f47926871beb1fa766ece5e27
Anastasiia <felitsias22@gmail.com>
2025-08-27 19:49:34 +0200

    Updated headerStyle constant with padding top

--- constants/height/header.ts
+++ constants/height/header.ts
@@ -5,3 +5,5 @@ export const HEADER_HEIGHT = (headerStyle.height as number) ?? 120;
 export const FOLDER_ITEM_HEIGHT = 80;
 export const FOLDER_BAR_PADDING_BOTTOM = 12;
 export const FOLDER_BAR_HEIGHT = FOLDER_ITEM_HEIGHT + FOLDER_BAR_PADDING_BOTTOM;
+
+export const PADDING_TOP = 110;
