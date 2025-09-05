6effb587e58587e69544616bbc872c110e44e9a2
Anastasiia <felitsias22@gmail.com>
2025-09-05 09:36:32 +0200

    Added TitleText component for styled text display

---
+++ ui/common/TitleText.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+export const TitleText = styled(Text)`
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: 0.3px;
+    color: #f2f2f2;
+`;
