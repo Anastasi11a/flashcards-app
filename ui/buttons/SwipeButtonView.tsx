eaf0795faacaa9c37dee31603cdc5385f68f31bd
Anastasiia <felitsias22@gmail.com>
2025-09-17 11:51:33 +0200

    Added SwipeButtonView component for customizable button with gradient background

---
+++ ui/buttons/SwipeButtonView.tsx
@@ -0,0 +1,55 @@
+import { Text, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import styled from "styled-components/native";
+
+import { titleText } from "@/constants/text/textStyles";
+
+export interface SwipeBtnProps {
+    iconName?: keyof typeof MaterialCommunityIcons.glyphMap;
+    label?: string;
+    gradientColors: [string, string];
+    onPress: () => void; 
+}
+
+const SwipeButtonView = ({ iconName, label, gradientColors, onPress }: SwipeBtnProps) => {
+    return (
+        <ButtonBackground colors={gradientColors}>
+            <PressableArea onPress={onPress}>
+                {iconName ? (
+                    <StyledIcon name={iconName} />
+                ) : label ? (
+                    <StyledText>{label}</StyledText>
+                ) : null}
+            </PressableArea>
+        </ButtonBackground >
+    ); 
+};
+
+export default SwipeButtonView;
+
+const ButtonBackground = styled(LinearGradient)`
+    width: 64px;
+    margin-left: 8px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+`;
+
+const PressableArea = styled(TouchableOpacity)`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+`;
+
+const StyledIcon = styled(MaterialCommunityIcons)`
+    font-size: 24px;
+    color: #e6e6e6;
+`;
+
+const StyledText = styled(Text)`
+    ${titleText}
+    text-align: center;
+`;
