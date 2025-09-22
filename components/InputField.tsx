a9fe7dbf4a107cbe5fd6e20d1844954cf1e3d03c
Anastasiia <felitsias22@gmail.com>
2025-09-22 23:49:56 +0200

    Removed unused components to clean up the UI

--- components/InputField.tsx
+++
@@ -1,6 +0,0 @@
-import InputWrapper, { InputWrapperProps } from "@/ui/InputWrapper";
-
-const InputField = (props: InputWrapperProps) => {
-    return <InputWrapper {...props} />;
-};
-export default InputField;
--- components/MenuPopupButton.tsx
+++
@@ -1,9 +0,0 @@
-import MenuPopupView, { MenuPopupViewProps } from "@/ui/MenuPopupView";
-
-type MenuPopupProps = MenuPopupViewProps;
-
-const MenuPopupButton = (props: MenuPopupProps) => {
-    return <MenuPopupView {...props} />;
-};
-
-export default MenuPopupButton;
