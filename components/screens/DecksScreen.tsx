412c18b0d03e923b203ab32fe3a0e9da3fb7bb65
Anastasiia <felitsias22@gmail.com>
2025-09-30 18:00:20 +0200

    Added DecksScreen component for displaying and managing decks with drag-and-drop functionality

---
+++ components/screens/DecksScreen.tsx
@@ -0,0 +1,36 @@
+import DraggableFlatList, { RenderItemParams } from "react-native-draggable-flatlist";
+
+import { Deck } from "@/data/decks";
+import { useDecks } from "@/context/DeckContext";
+import DeckContainer from "@/ui/container/DeckContainer";
+import { navigateToDecks } from "@/utils/navigation/navigation";
+import { flatListStyles } from "@/utils/contentContainerStyle";
+
+const DecksScreen = () => {
+    const { decks, actions } = useDecks();
+
+    const handlePress = (deckId: string) => {
+        navigateToDecks(deckId);
+    };
+
+    const renderDeckItem = ({ item, drag }: RenderItemParams<Deck>) => (
+        <DeckContainer
+            title={item.title}
+            cardCount={item.cards.length}
+            onPress={() => handlePress(item.id)}
+            onLongPress={drag}
+        />
+    );
+
+    return (
+        <DraggableFlatList
+            data={decks}
+            keyExtractor={(item) => item.id}
+            renderItem={renderDeckItem}
+            onDragEnd={({ data }) => actions.reorderDecks(data)}
+            contentContainerStyle={flatListStyles.appDecks()}
+        />
+    );
+};
+
+export default DecksScreen;
