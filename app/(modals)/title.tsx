76caf528e099bf2ffe1147a5b30a32151985b8ad
Anastasiia <felitsias22@gmail.com>
2025-09-30 17:39:27 +0200

    Added TitleModal component for managing title creation and editing

---
+++ app/(modals)/title.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+
+import AddTitle from "@/components/pages/AddTitle";
+import useCustomHeader from "@/hooks/useCustomHeader";
+import { useEditTitleState } from "@/hooks/useEditTitleState";
+import { useSubmitDraftTitle } from "@/hooks/useSubmitDraftTitle";
+import { useSaveModal } from "@/hooks/useSaveModal";
+import { TitleParams } from "@/utils/navigation/navigation";
+
+const TitleModal = () => {
+    const [draftTitle, setDraftTitle] = useState('');
+
+    const { deckId, mode } = useLocalSearchParams<TitleParams>();
+    const isEdit = mode === 'edit';
+    const isFolder = mode === 'create-folder';
+
+    const editState = useEditTitleState({ deckId });
+    const { submit } = useSubmitDraftTitle();
+    const { onSave } = useSaveModal(editState.save);
+
+    const handleSave = async () => {
+        if (isEdit) {
+            onSave();
+        } else if (isFolder) {
+            await submit('folder', draftTitle);
+            setDraftTitle('');
+        } else {
+            await submit('deck', draftTitle);
+            setDraftTitle('');
+        }
+    };
+
+    useCustomHeader({
+        title: isEdit 
+            ? 'Edit Title'
+            : isFolder
+            ? 'Folder Title' 
+            : 'Deck Title',
+        rightButton: {
+            label: isEdit ? 'Done' : 'Next',
+            onPress: handleSave,
+        },
+    });
+
+    return (
+        <AddTitle
+            mode={mode}
+            editState={editState}
+            draftTitle={draftTitle}
+            setDraftTitle={setDraftTitle}
+        />
+    );
+};
+
+export default TitleModal;
