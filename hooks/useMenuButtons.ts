5c4e8b2acd06bfcf96955e0781d9e0b3c95d4e39
Anastasiia <felitsias22@gmail.com>
2025-10-13 18:50:04 +0200

    Refactored useDeckMenuButtons to useMenuButtons for improved flexibility and added support for folder type

---
+++ hooks/useMenuButtons.ts
@@ -0,0 +1,81 @@
+import { createElement } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export type SortOrder = 'asc' | 'desc';
+
+interface CommonMenuProps {
+    id: string;
+    type: 'deck' | 'folder';
+    onEditTitle: () => void;
+    onDelete: (id: string) => void;
+}
+
+interface DeckExtras {
+    onAdd?: () => void;
+    onExport?: () => void;
+    onSort?: () => void;
+    currentSortOrder?: SortOrder;
+}
+
+type MenuProps = CommonMenuProps & Partial<DeckExtras>;
+
+export const useMenuButtons = ({
+    id,
+    type,
+    onAdd,
+    onEditTitle,
+    onExport,
+    onDelete,
+    onSort,
+    currentSortOrder,
+}: MenuProps) => {
+    const icon = (
+        name: keyof typeof MaterialCommunityIcons.glyphMap,
+        color = '#fff'
+    ) => createElement(MaterialCommunityIcons, { name, size: 24, color });
+
+    const buttons = [];
+
+    if (type === 'deck' && onAdd) {
+        buttons.push({
+            label: 'Add Card',
+            icon: icon('playlist-plus'),
+            onPress: onAdd,
+        });
+    }
+
+    buttons.push({
+        label: 'Rename title',
+        icon: icon('playlist-edit'),
+        onPress: onEditTitle,
+    });
+
+    if (type === 'deck' && onSort && currentSortOrder) {
+        buttons.push({
+            label: 'Sort',
+            icon: icon(
+                currentSortOrder === 'asc'
+                ? 'sort-alphabetical-ascending-variant'
+                : 'sort-alphabetical-descending-variant'
+            ),
+            onPress: onSort,
+        });
+    }
+
+    if (type === 'deck' && onExport) {
+        buttons.push({
+            label: 'Export',
+            icon: icon('export-variant'),
+            onPress: onExport,
+        });
+    }
+
+    buttons.push({
+        label: 'Remove',
+        icon: icon('delete-sweep', '#ff4d4f'),
+        onPress: () => onDelete(id),
+        isDestructive: true,
+    });
+
+    return buttons;
+};
